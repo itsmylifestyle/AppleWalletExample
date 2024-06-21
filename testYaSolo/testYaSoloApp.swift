1cf2c49b37f1b11b61692beeba22d1d84e98b9cf
itsmylifestyle <hahaii00@mail.ru>
2024-06-21 19:07:51 +0500

    Initial Commit

---
+++ testYaSolo/testYaSoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  testYaSoloApp.swift
+//  testYaSolo
+//
+//  Created by Айбек on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct testYaSoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
